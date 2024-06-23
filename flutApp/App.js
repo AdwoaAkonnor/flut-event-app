f35d4d37ab90fad9ea09dfc61d386d70d7949f50
AdwoaAkonnor <aaakonnor001@st.ug.edu.gh>
2024-06-23 18:59:40 +0000

    first task completed

--- flutApp/App.js
+++ flutApp/App.js
@@ -1,50 +1,27 @@
-import { View, Text, SafeAreaView , Image, TouchableOpacity } from 'react-native'
+
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
-const App = () => {
+const SplashScreen = () => {
   return (
-    <SafeAreaView>
-    <View style={{  justifyContent : 'center'}}>
-      <Text style={{ color : '#000000', fontWeight: 700 , fontSize : 32, alignSelf: 'center', marginTop : 80}}>Welcome!</Text>
-      <Text style={{ color : '#7C7C7C', fontSize: 16, alignSelf: 'center', fontWeight : 500}}>sign in or create a new account</Text>
-      <View style={{ alignSelf: 'center', marginTop: 100}}>
-        <Image source={require('./assets/Frame.png')}/>
-      </View>
-    <View>
-      <TouchableOpacity style={{ 
-        backgroundColor : '#0DCDAA',
-         paddingVertical : 20,
-         paddingHorizontal: 45,
-         marginRight:30,
-         marginLeft: 30,
-          borderRadius: 7, 
-          alignItems : 'center' ,
-           marginTop: 100 ,
-            justifyContent  : 'center'}}>
-<Text style={{color : 'white',fontWeight: 500, fontSize : 16}}>sign in </Text>
-      </TouchableOpacity>
+    <SafeAreaView 
+    style={{backgroundColor : '#0DCDAA'
+     , flex: 1
+     }}>
+    <View style={{ flex :1 ,
+       justifyContent : 'center'
+       }}>
+      <Text 
+      style={{
+        alignSelf :'center',
+         color :'#FFFFFF',
+         fontSize: '100px',
+         fontWeight: 'bold',
+         fontFamily : 'skia'
+         }}>Flut</Text>
     </View>
-    <View>
-      <TouchableOpacity style={{ 
-        borderWidth : 1,
-        borderColor : 'CCCCCC',
-         paddingVertical : 20,
-         paddingHorizontal: 30,
-         marginRight:30,
-         marginLeft: 30,
-          borderRadius: 7, 
-          alignItems : 'center' ,
-           marginTop: 10 ,
-            justifyContent  : 'center',
-            flexDirection : 'row'}}>
-<Text style={{color : '#7C7C7C',fontWeight: 500, fontSize : 16}}> No account yet? </Text>
-<Text style={{color: '#0DCDAA', fontWeight: 500, fontSize: 16}}>sign up?</Text>
-      </TouchableOpacity>
-    </View>
-    </View>
-    
     </SafeAreaView>
   )
 }
 
-export default App
+export default SplashScreen 
